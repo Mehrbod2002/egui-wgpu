ab02972f616a3d005dc1176d70973d24d2ce1ce9
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2024-12-14 19:55:16 +0330

    fea
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

---
+++ target/debug/deps/egui_wgpu-5b90a9ad79cbde80.d
@@ -0,0 +1,9 @@
+/home/mob/projects/rusts/egui-wgpu/target/debug/deps/libegui_wgpu-5b90a9ad79cbde80.rmeta: src/lib.rs src/renderer.rs src/capture.rs src/egui.wgsl src/texture_copy.wgsl
+
+/home/mob/projects/rusts/egui-wgpu/target/debug/deps/egui_wgpu-5b90a9ad79cbde80.d: src/lib.rs src/renderer.rs src/capture.rs src/egui.wgsl src/texture_copy.wgsl
+
+src/lib.rs:
+src/renderer.rs:
+src/capture.rs:
+src/egui.wgsl:
+src/texture_copy.wgsl:
